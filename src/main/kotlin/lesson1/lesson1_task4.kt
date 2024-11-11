4be476f15b6ec7d8282236292bbccfa0c44aa967
Vasilii Krinitsyn <vadzik25@gmail.com>
2024-11-11 21:17:20 +0500

    KS-1-3 Complete task 3 lesson 1

---
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+fun main() {
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075F
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println("Расстояние, которое пролетел Гагарин пока был в космосе в миллиметрах $length")
+    println("Возраст Гагарина на момент полета $age")
+    println("Часть дня Гагарин провел в космосе $partOfDay")
+    println("Количество секунд которое Гагарин провел в космосе $seconds")
+    println("Часть года Гагарин провел в космосе $partOfYear")
+    println("Апогей орбиты Гагарина в метрах $apogee")
+}
