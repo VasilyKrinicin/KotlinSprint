5be75ac9881bb7b6ae53a7a41583089626d18a59
Vasilii Krinitsyn <vadzik25@gmail.com>
2024-11-11 20:18:26 +0500

    KS-1-3 Complete task 3 lesson 1

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson1
+
+fun main() {
+    val yearOfFlight =1961
+    var hourOfFlight= 9
+    var minuteOfFlight= 7
+    println("Год полета: $yearOfFlight")
+    println ("Час взлета: $hourOfFlight")
+    println("Минуты взлета: $minuteOfFlight")
+
+    hourOfFlight=10
+    minuteOfFlight=55
+    println("Время приземления $hourOfFlight:$minuteOfFlight")
+
+}
