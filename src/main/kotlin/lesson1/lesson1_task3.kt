813287a887068f5784f012ca50bc1efbc6ecc694
Vasilii Krinitsyn <vadzik25@gmail.com>
2024-11-12 17:49:01 +0500

    KS-1-3 Refactor lesson1 task3

--- src/main/kotlin/lesson1/lesson1_task3.kt
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -1,15 +1,15 @@
 package org.example.lesson1
 
 fun main() {
-    val yearOfFlight =1961
-    var hourOfFlight= 9
-    var minuteOfFlight= 7
+    val yearOfFlight = "1961"
+    var hourOfFlight = "09"
+    var minuteOfFlight = "07"
     println("Год полета: $yearOfFlight")
-    println ("Час взлета: $hourOfFlight")
+    println("Час взлета: $hourOfFlight")
     println("Минуты взлета: $minuteOfFlight")
 
-    hourOfFlight=10
-    minuteOfFlight=55
+    hourOfFlight = "10"
+    minuteOfFlight = "55"
     println("Время приземления $hourOfFlight:$minuteOfFlight")
 
 }
